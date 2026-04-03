bf8dd9b0d93141ff72f2989c3f555db3ad3a0a47
Md Inzamam Ul Khan <inzamamkhan618@gmail.com>
2026-04-03 06:09:34 +0600

    added react icons goRightArro and changed its rotation
    with that the navbar is complete

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import navLogo from "../../assets/image/Webbriks logo-ai 1.png";
 import { NavLink } from "react-router";
+import navLogo from "../../assets/image/Webbriks logo-ai 1.png";
+import { GoArrowRight } from "react-icons/go";
 const NavBar = () => {
   return (
     <div className="bg-[#FEF9EF]">
       <div className="flex justify-between items-center w-10/12 mx-auto py-9 ">
         <img src={navLogo} alt="NavBar Logo" />
-        <div className="flex items-center gap-24">
+        <div className="flex items-center gap-22.25">
           <nav className="text-[#2B2121] flex gap-8" >
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about-us">About us</NavLink>
@@ -14,7 +14,7 @@ const NavBar = () => {
             <NavLink to="/portfolio">Portfolio</NavLink>
             <NavLink to="/contacts">Contact</NavLink>
                   </nav>
-                  <button className="btn btn-primary">Get Started</button>
+                  <button className="btn btn-primary py-7 px-9 text-xl">Get Started <GoArrowRight className="rotate-330" /></button>
         </div>
       </div>
     </div>
